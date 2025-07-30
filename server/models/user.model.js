9df1f047492ee714ba85b57e1aaee918adcdbe92
Sahanilesh <sahaniniles18@gmail.com>
2025-07-31 01:12:29 +0530

    Auth || Models and controller is created for users

---
+++ server/models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { use } from "react";
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required : true},
+    email: {type: String, required : true, unique: true},
+    password: {type: String, required : true},
+    verifyOtp: {type: String, default: ''},
+    verifyOtpExpireAt: {type: Number, default: 0},
+    isAccountVerified: {type: Boolean, default: false},
+    resetOtp: {type: String, default: ''},
+    resetOtpExpireAt: {type: Number, default: 0},
+},{timestamps:true});
+
+const userModel = mongoose.models.users || mongoose.model('user', userSchema )
+
+export default userModel;
